e44d4d8746837473f9d5b15719da1261fa85f9b0
XamLe <max.lewerenz@googlemail.com>
2014-02-08 12:13:39 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'f6f0ba852c935df98c1bb87f899113d5f8c4b82cdaf8e29d7775b931d139dd71e284d58ea7c4a22bf5717c3389721474d433583c5543ecf8671407af9c87657b'
